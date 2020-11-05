7f1347f16aa7813ac59fac716864a37fb1183cc3
vipisanan <vipisanan1@gmail.com>
2020-11-05 11:10:03 +0530

    remove token

--- themes/twentyseventeen/index.php
+++ themes/twentyseventeen/index.php
@@ -40,7 +40,6 @@ get_header(); ?>
             "x-rapidapi-key: SIGN-UP-FOR-KEY"
         ),
     ));
-//    token : IGQVJYMGtJdGg2djBibWxjUm9famRyTExoWThfdElGXzMxMHM0amZAJaFpxd0pidVhuN3k2UDdpNVJ5SHplc1g3VXc0T3FXTVFkSTFmU3JocWNkaEU4LTBVSjhMamFINkJfM2NjcEVYTkFJQ09VSmE4ZAAZDZD
 
 
     $response = curl_exec($curl);
