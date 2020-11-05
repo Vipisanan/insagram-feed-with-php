c00787a79257e95cb91cfeb012c3c085a9a19495
vipisanan <vipisanan1@gmail.com>
2020-11-05 21:53:21 +0530

    create child theme for api

--- themes/twentyseventeen-child/functions.php
+++ themes/twentyseventeen-child/functions.php
@@ -1,5 +1,19 @@
 <?php
+// style
+function theme_enqueue_styles()
+{
+    $parent_style = 'parent-style';
+    wp_enqueue_style($parent_style, get_template_directory_uri() . '/style.css');
+    wp_enqueue_style('child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array($parent_style)
+    );
+}
 
+add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
+
+
+//-------------------------------------------api----------------------------
 add_action('rest_api_init', function () {
     register_rest_route( 'wl/v1', 'latest-posts/(?P<category_id>\d+)',array(
         'methods'  => 'GET',
