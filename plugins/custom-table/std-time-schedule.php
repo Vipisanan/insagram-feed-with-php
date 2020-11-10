6e25a7465a1a1c37c06980ee2b673ab75716fff2
vipisanan <vipisanan1@gmail.com>
2020-11-10 14:14:19 +0530

    rooms , invoice and promo code table create and schedule table update

--- plugins/custom-table/std-time-schedule.php
+++ plugins/custom-table/std-time-schedule.php
@@ -5,23 +5,6 @@ Description: Create a custom table
 Version: 0.1.2
 Author: Vipisanan
 */
-//global $wpdb;
-//
-//$charset_collate = $wpdb->get_charset_collate();
-//
-//$sql = "CREATE TABLE `{$wpdb->base_prefix}cli_logins` (
-//      public_key varchar(191) NOT NULL,
-//      private_key varchar(191) NOT NULL,
-//      user_id bigint(20) UNSIGNED NOT NULL,
-//      created_at datetime NOT NULL,
-//      expires_at datetime NOT NULL,
-//      PRIMARY KEY  (public_key)
-//    ) $charset_collate;";
-//
-//require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-//dbDelta($sql);
-
-
 
 
 global $wpdb;
@@ -38,16 +21,48 @@ $sql = "CREATE TABLE `{$wpdb->base_prefix}std_user` (
     created_at datetime NOT NULL
 ) $charset_collate;";
 
-$sql2 = "CREATE TABLE `{$wpdb->base_prefix}std_booking_schedule` (
-  id bigint(20) AUTO_INCREMENT PRIMARY KEY,
-  std_user_id bigint(20) NOT NULL,
-  schedule datetime NOT NULL,
-  created_at datetime NOT NULL,
-  FOREIGN KEY (std_user_id) REFERENCES `{$wpdb->base_prefix}std_user`(id)
-)$charset_collate;";
+$rooms = "CREATE TABLE `{$wpdb->base_prefix}std_rooms` (
+                id bigint(20) AUTO_INCREMENT PRIMARY KEY,
+                name VARCHAR(30),
+                address VARCHAR(50),
+                email VARCHAR(50),
+                created_at datetime NOT NULL
+            )$charset_collate;";
+
+$invoice = "CREATE TABLE `{$wpdb->base_prefix}std_invoices` (
+            id bigint(20) AUTO_INCREMENT PRIMARY KEY,
+            std_user_id bigint(20) NOT NULL,
+            total_amount float NOT NULL,
+            booking_type VARCHAR(30) NOT NULL,
+            no_of_hours VARCHAR(10) NOT NULL,
+            FOREIGN KEY (std_user_id) REFERENCES `{$wpdb->base_prefix}std_user`(id)
+            )$charset_collate;";
+
+$schedule = "CREATE TABLE `{$wpdb->base_prefix}std_booking_schedule` (
+              id bigint(20) AUTO_INCREMENT PRIMARY KEY,
+              std_user_id bigint(20) NOT NULL,
+              room_id bigint(20) NOT NULL,
+              invoice_id bigint(20) NOT NULL,
+              schedule datetime NOT NULL,
+              created_at datetime NOT NULL,
+              FOREIGN KEY (std_user_id) REFERENCES `{$wpdb->base_prefix}std_user`(id),
+              FOREIGN KEY (room_id) REFERENCES `{$wpdb->base_prefix}std_rooms`(id),
+              FOREIGN KEY (invoice_id) REFERENCES `{$wpdb->base_prefix}std_invoices`(id)
+        )$charset_collate;";
+
+
+$promo_code = "CREATE TABLE `{$wpdb->base_prefix}std_promo_code` (
+                id bigint(20) AUTO_INCREMENT PRIMARY KEY,
+                promo_type VARCHAR(30) NOT NULL,
+                promo_code VARCHAR(50) NOT NULL,
+                discount bigint(20)
+                )$charset_collate;";
 
 
 require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
 dbDelta($sql);
-dbDelta($sql2);
-//
+dbDelta($rooms);
+dbDelta($invoice);
+dbDelta($schedule);
+dbDelta($promo_code);
+
